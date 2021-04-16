d08435f69e9221b6fe7e9e699edde8573ba51c8e
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-16 16:03:52 -0300

    meal schema set

---
+++ lib/mealixir/meal.ex
@@ -0,0 +1,24 @@
+defmodule Mealixir.Meal do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:description, :date, :calories]
+
+  schema "meals" do
+    field :description, :string
+    field :date, :utc_datetime
+    field :calories, :integer
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_number(:calories, greater_than_or_equal_to: 0)
+  end
+end
