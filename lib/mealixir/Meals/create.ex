ac8b898b3e2071e8426f3ebace12bf1a4f25943f
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-16 16:16:53 -0300

    meal creation first set, error will be handled yet

--- lib/mealixir/Meals/create.ex
+++ lib/mealixir/Meals/create.ex
@@ -1,2 +1,14 @@
-defmodule Mealivery.Meals.Create do
+defmodule Mealixir.Meals.Create do
+  alias Mealixir.{Meal, Repo}
+
+  def call(params) do
+    params
+    |> Meal.changeset()
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %Meal{}} = result), do: result
+
+  defp handle_insert({:error, _result}), do: {:error, "Something went wrong"}
 end
