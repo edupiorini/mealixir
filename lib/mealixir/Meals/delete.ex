f351e47e7458a9466baf1bc4c493df723273d45b
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-19 05:21:55 -0300

    casting was missing, no id format is veryfied

--- lib/mealixir/Meals/delete.ex
+++ lib/mealixir/Meals/delete.ex
@@ -1,7 +1,15 @@
 defmodule Mealixir.Meals.Delete do
+  alias Ecto.UUID
   alias Mealixir.{Meal, Repo}
 
   def call(id) do
+    case UUID.cast(id) do
+      :error -> {:error, "Invalid ID format!"}
+      {:ok, uuid} -> delete(uuid)
+    end
+  end
+
+  defp delete(id) do
     case Repo.get(Meal, id) do
       nil -> {:error, "Meal not found"}
       meal -> Repo.delete(meal)
--- lib/mealixir/Meals/get.ex
+++ lib/mealixir/Meals/get.ex
@@ -1,7 +1,15 @@
 defmodule Mealixir.Meals.Get do
+  alias Ecto.UUID
   alias Mealixir.{Meal, Repo}
 
   def call(id) do
+    case UUID.cast(id) do
+      :error -> {:error, "Invalid ID format!"}
+      {:ok, uuid} -> get(uuid)
+    end
+  end
+
+  defp get(id) do
     case Repo.get(Meal, id) do
       nil -> {:error, "Meal not found"}
       meal -> {:ok, meal}
