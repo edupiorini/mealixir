d7750da520579844a3fc46155be1fe7f201569fc
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-20 07:49:44 -0300

    facade to show all set, but still not working

--- lib/mealixir.ex
+++ lib/mealixir.ex
@@ -1,8 +1,9 @@
 defmodule Mealixir do
-  alias Mealixir.Meals.{Create, Delete, Get, Update}
+  alias Mealixir.Meals.{Create, Delete, Get, GetAll, Update}
 
   defdelegate create_meal(params), to: Create, as: :call
   defdelegate delete_meal(id), to: Delete, as: :call
   defdelegate show_meal(id), to: Get, as: :call
+  defdelegate show_all(), to: GetAll, as: :call
   defdelegate update_meal(params), to: Update, as: :call
 end
