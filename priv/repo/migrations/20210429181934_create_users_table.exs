e2be1bcbaf17ede157e35b6aabb8575cdb7ed76f
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-05-03 05:43:20 -0300

    migrations done

---
+++ priv/repo/migrations/20210429181934_create_users_table.exs
@@ -0,0 +1,14 @@
+defmodule Mealixir.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table :users do
+      add :name, :string
+      add :email, :string
+      add :cpf, :string
+
+      timestamps()
+    end
+
+  end
+end
