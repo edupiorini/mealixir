5c354da6a9256a4c9c2df7e174f43f69c416fc40
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-05-04 05:07:34 -0300

    facade for user creation done

--- lib/mealixir.ex
+++ lib/mealixir.ex
@@ -1,9 +1,12 @@
 defmodule Mealixir do
   alias Mealixir.Meals.{Create, Delete, Get, GetAll, Update}
+  alias Mealixir.Users.Create, as: CreateUser
 
   defdelegate create_meal(params), to: Create, as: :call
   defdelegate delete_meal(id), to: Delete, as: :call
   defdelegate show_meal(id), to: Get, as: :call
   defdelegate show_all(), to: GetAll, as: :call
   defdelegate update_meal(params), to: Update, as: :call
+
+  defdelegate create_user(params), to: CreateUser, as: :call
 end
