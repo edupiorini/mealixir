de1d2c8c29d53430fad68de6c56e087e5d2d4e64
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-05-04 05:08:07 -0300

    user creation set

---
+++ lib/mealixir/Users/create.ex
@@ -0,0 +1,16 @@
+defmodule Mealixir.Users.Create do
+  alias Mealixir.{Error, Repo, User}
+
+  def call(params) do
+    params
+    |> User.changeset()
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %User{}} = result), do: result
+
+  defp handle_insert({:error, result}) do
+    {:error, Error.build(:bad_request, result)}
+  end
+end
